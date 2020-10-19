508b8cfe848cfc09044e5adbeb3185d487a7cbc2
my828 <minyang828@gmail.com>
2020-10-18 21:00:07 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sharespace/precache-manifest.9a131ab1152907b32796aa3a2ae1e8c0.js"
+  "/sharespace/precache-manifest.12f9a790f05b8644912e1f2494200c9a.js"
 );
 
 self.addEventListener('message', (event) => {
